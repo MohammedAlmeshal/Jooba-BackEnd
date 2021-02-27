7582a83fc406432f2e9defc84fa6809f69f09c89
Mohammed <md.almeshal@gmail.com>
2021-02-27 19:23:50 +0300

    :sparkles: Home page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -6,6 +6,7 @@ import {
   Heading,
   Center,
   useColorMode,
+  Image
 } from "@chakra-ui/react";
 import LogoLight from "../public/logo.svg";
 import LogoDark from "../public/logoDark.svg";
@@ -18,15 +19,15 @@ const Home = () => {
     <Center
       h="100vh"
       style={{ backgroundImage: `url(${Background})` }}
-      w="100%"
+
       pt="7rem"
     >
       <Flex justify="center" align="center" w="90%" wrap="wrap">
-        <Heading textAlign="center" fontWeight="800" fontSize="8xl" as="h1">
-          Got <br /> a <br /> question
+        <Heading textAlign="center" fontWeight="800" fontSize={['6xl','6xl','8xl']} as="h1" mb='2rem' >
+          Got a<br /> question ?
         </Heading>
 
-        <img width="700px" src={Logo}></img>
+        <Image me='1rem' width="700px" src={Logo}></Image>
       </Flex>
     </Center>
   );
