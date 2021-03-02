ccc9afb8a4890ed4c11a0899d858fec1d57944cc
Mohammed <md.almeshal@gmail.com>
2021-03-02 15:49:02 +0300

    🌐 implemented i18n

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,7 @@ const config = require("config");
 const jwt = require("jsonwebtoken");
 
 const User = require("../../models/User");
+const Post = require("../../models/Post");
 
 // @route POST api/users
 // @desc Rigister new users
@@ -33,28 +34,47 @@ router.post("/", (req, res) => {
       bcrypt.hash(newUser.password, salt, (err, hash) => {
         if (err) throw err;
         newUser.password = hash;
-        newUser.save().then((user) => {
-          // create token and expire in 1 hour
-          jwt.sign(
-            {
-              id: user.id,
-            },
-            config.get("jwtSecret"),
-            { expiresIn: 3600 },
-            (err, token) => {
-              if (err) throw err;
-              res.json({
-                token: token,
-                user: {
-                  id: user.id,
-                  name: user.name,
-                  username: user.username,
-                  email: user.email,
-                },
-              });
-            }
-          );
+        // create first question
+        const newPost = new Post({
+          question: {
+            questionTxt: 'This is your first question! you can choose to reply or to ignore...',
+          },
+          questionTo: newUser,
         });
+  
+        newPost.save().then((post) => {
+          newUser.posts.push(post);
+      
+          newUser.save().then((user) => {
+            // create token and expire in 1 hour
+            jwt.sign(
+              {
+                id: user.id,
+              },
+              config.get("jwtSecret"),
+              { expiresIn: 3600 },
+              (err, token) => {
+                if (err) throw err;
+                res.json({
+                  token: token,
+                  user: {
+                    id: user.id,
+                    name: user.name,
+                    username: user.username,
+                    email: user.email,
+                  },
+                });
+              }
+            );
+          });
+
+
+
+        });
+
+
+
+       
       });
     });
   });
