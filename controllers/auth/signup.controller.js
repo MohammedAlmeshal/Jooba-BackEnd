8aaa4332ce78bbde3adc6f5802c179b1b41f8a36
Mohammed <md.almeshal@gmail.com>
2021-07-03 17:31:09 +0300

    refactor: auth controllers

---
+++ controllers/auth/signup.controller.js
@@ -0,0 +1,20 @@
+const services = require("../../services/auth");
+const { ErrorHandler } = require("../../utils/errorHandler");
+
+const signup = async (req, res, next) => {
+  try {
+  
+    const { name, username, email, password } = req.body;
+    if (!name || !username || !email || !password) {
+      throw new ErrorHandler(404, "Missing required fields");
+    } else {
+      const token = await services.signup({ name, username, email, password });
+      res.json({ token });
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+module.exports = signup;
---
+++ controllers/posts/index.js
@@ -0,0 +1,9 @@
+const answerQuestion = require("./answerQuestion.controller");
+const ignoreQuestion = require("./ignoreQuestion.controller");
+const askQuestion = require("./askQuestion.controller");
+
+module.exports = {
+  answerQuestion,
+  ignoreQuestion,
+  askQuestion,
+};
