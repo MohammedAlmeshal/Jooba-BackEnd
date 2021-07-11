c1f007b65dd453139630b0519331fcf384c986da
Mohammed <md.almeshal@gmail.com>
2021-07-12 00:35:04 +0300

    fix: removed imports

--- services/auth/login.service.js
+++ services/auth/login.service.js
@@ -1,5 +1,4 @@
 const bcrypt = require("bcryptjs");
-const config = require("config");
 const jwt = require("jsonwebtoken");
 const User = require("../../models/User");
 const { ErrorHandler } = require("../../utils/errorHandler");
